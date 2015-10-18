781000a888619b4ea18e28dfb9a7573745c3b9db
Guti Benjámin <guti.benjamin94@gmail.com>
2015-10-18 02:00:30 +0200

    Létrehoztam egy kiíratásért felelős osztály. Az osztályban megtalálható az amőba kiíratásáért felelős metódus

--- src/first_project/Main.java
+++ src/first_project/Main.java
@@ -2,10 +2,20 @@ package first_project;
 
 public class Main {
 
+	static char[][] field = new char[3][3];
+
 	public static void main(String[] args) {
-		Valami o = new Valami();
-		
-		o.kiir();
+		PrintToScreen screenPrinter = new PrintToScreen();
+
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				field[i][j] = '*';
+
+			}
+		}
+		field[0][0] = 'X';
+		field[1][1] = 'X';
+		screenPrinter.PrintTicTacToeToScreen(field);
 	}
 
 }
---
+++ src/first_project/PrintToScreen.java
@@ -0,0 +1,21 @@
+package first_project;
+
+public class PrintToScreen {
+    public PrintToScreen()
+    {
+
+    }
+
+    public void PrintTicTacToeToScreen(char[][] field)
+    {
+        System.out.println("|---1.--2.--3.--");
+        System.out.println("|-|-----------||");
+        System.out.println("1.|-"+  field[0][0]+"---"+  field[0][1]+"---"+  field[0][2]+"-|| ");
+        System.out.println("|-|-----------||");
+        System.out.println("2.|-"+  field[1][0]+"---"+  field[1][1]+"---"+  field[1][2]+"-|| ");
+        System.out.println("|-|-----------||");
+        System.out.println("3.|-"+  field[2][0]+"---"+  field[2][1]+"---"+  field[2][2]+"-|| ");
+        System.out.println("|-|-----------||");
+
+    }
+}
